1562e082b6df9ba7a04311739e6cdd2adfb88531
jonathan <jonathan@adeogroup.co.uk>
2016-11-19 12:29:33 +0000

    remove sublimetext

--- config/sublime-text-3/Packages/User/git-commit-and-push.py
+++
@@ -1,9 +0,0 @@
-import sublime, sublime_plugin
-
-class GitCommitAndPushCommand(sublime_plugin.WindowCommand):
-    def run(self, scope=""):
-        self.window.run_command("git_quick_add");
-        self.pause;
-        self.window.run_command("git_quick_commit");
-        self.pause;
-        self.window.run_command("git_push");
